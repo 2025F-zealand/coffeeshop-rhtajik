1ffa514220d48d60bb5edf09fec1e8f3707a3e9d
rhtajik <144002592+rhtajik@users.noreply.github.com>
2025-02-13 01:33:12 +0100

    oprette alle klassene

---
+++ CoffeeShopConsoleAppNet60/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public abstract class Coffee
+    {
+        public abstract int Price();
+       
+
+        public abstract string Strength();
+
+    }
+}
